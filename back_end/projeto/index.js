d8f6d622d205683b91729c9706b1ec62f5e996d1
pam1277 <pamellagabriela503@gmail.com>
2026-04-14 08:26:57 -0300

    index.js
    Initial project setup with basic information logged.

---
+++ back_end/projeto/index.js
@@ -0,0 +1,12 @@
+/*************************************************************
+Resumo do projeto de Node.js
+
+*******************************************************************************/
+console.log('cabeçalho projeto vem aqui')
+console.log('Nome da escola: Escola do futuro do Estado de Goiás - José Luiz Bittencourt');
+console.log('Nome do curso: Desenvolvimento web e cybersegurança')
+console.log('Nome da disciplina: Back-End')
+console.log('Nome do professor: George Mendes marra')
+console.log('Título do projeto: chat simples dm tempo real')
+console.log('Integrantes do Projeto: Maria Eduarda , Maúrico, Nathali, Pâmella, Rebeca ')
+
